e0747e7ead33111a2f7c02d6675b74c6192eca16
Florian-THP <triboutflorian@outlook.com>
2024-11-04 16:53:25 +0100

    gmail

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,13 @@
 # Load the Rails application.
 require_relative "application"
 
-ActionMailer::Base.smtp_settings = {
-  user_name: ENV["MAILJET_LOGIN"],
-  password: ENV["MAILJET_PWD"],
-  domain: "xuniverse.fr",
-  address: "in-v3.mailjet.com",
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
+ActionMailer::Base.smtp_settings =   {
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :domain             => 'gmail.com', #you can also use google.com
+  :authentication     => :plain,
+  :user_name          => ENV['GMAIL_LOGIN'],
+  :password           => ENV['GMAIL_PWD']
 }
 # Initialize the Rails application.
 Rails.application.initialize!
